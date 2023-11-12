154db8b760262714a0c9213f7eb6c790be4fd73f
ajoygp <41632101+ajoygp@users.noreply.github.com>
2023-11-12 22:57:10 +0530

    add favicons + minor refactoring

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Greet Genie</title>
-        <link rel="icon" href="/genie.jpg" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
