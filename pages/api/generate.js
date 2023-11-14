a1f83bc581a2248197e5246b89ddb3c30d22c82f
ajoygp <41632101+ajoygp@users.noreply.github.com>
2023-11-14 09:03:02 +0530

    change openai model to 3.5 turbo

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,13 +1,11 @@
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
-
 export default async function (req, res) {
-  if (!configuration.apiKey) {
+  if (!openai.apiKey) {
     res.status(500).json({
       error: {
         message: 'OpenAI API key not configured',
@@ -56,31 +54,40 @@ export default async function (req, res) {
   if (traitOne == '' || traitTwo == '') {
     res.status(400).json({
       error: {
-        message: 'Please enter valid traitss',
+        message: 'Please enter valid traits',
       },
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: generatePrompt(
-        name,
-        company,
-        occasion,
-        role,
-        number,
-        traitOne,
-        traitTwo,
-        like
-      ),
+    let generatedPrompt = generatePrompt(
+      name,
+      company,
+      occasion,
+      role,
+      number,
+      traitOne,
+      traitTwo,
+      like
+    );
+
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: generatedPrompt,
+        },
+      ],
       temperature: temperature,
       max_tokens: 250,
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+
+    res
+      .status(200)
+      .json({ result: completion.choices[0]['message']['content'] });
   } catch (error) {
-    // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
